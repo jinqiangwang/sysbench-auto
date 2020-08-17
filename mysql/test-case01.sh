a13987d2c6bedf65b91ca3cf3b655930536d7423
Andy Wang <andy.wang@scaleflux.com>
2020-08-17 13:42:41 +0800

    add wrapper script for mysql8.0 test

---
+++ mysql/test-case01.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export dev_name=sfdv0n1
+export prep_dev=yes     # yes|no
+export init_db=yes      # yes|no
+export atomic_write=0   # 0|1
+export WORKLOADS=sb/mysql-8.0
+############################################################
+## test configuraion for quick verification
+export workload_set="prepare oltp_read_only oltp_insert"
+export run_time=30
+export thread_count_list="1 4"
+export table_count=60
+export table_size=150000
+## 600 table x 15 million records for 2T data set
+# export workload_set="prepare oltp_read_only oltp_insert oltp_update_index oltp_update_non_index oltp_read_write oltp_write_only"
+# export run_time=1200
+# export thread_count_list="1 4 16 64 256 512 1024 2048"
+# export table_count=600
+# export table_size=15000000
+############################################################
+export innodb_buffer_pool_size=32G
+export iostat_dev_str=${dev_name}
+export table_data_src_file=""   # empty|../compress/best.txt 
+export run_cmd_script=./run-cases.sh
+
+export WORKSPACE=./
+
+if [ ! -d "./sysbench-auto" ]; 
+then
+    git clone https://github.com/jinqiangwang/sysbench-auto.git
+    cd ./sysbench-auto
+else
+    cd ./sysbench-auto
+    git pull
+fi
+
+cd ./sysbench
+
+tar xzf bin.tgz
+tar xzf compress.tgz
+
+cd ./mysql
+
+export cfg_list=${WORKLOADS}
+${run_cmd_script}
